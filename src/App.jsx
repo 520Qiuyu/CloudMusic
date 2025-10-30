3cc48712f505843bab4af2bcca7926ee6cd613c7
邱宇 <71555151+520Qiuyu@users.noreply.github.com>
2025-10-30 22:47:36 +0800

    refactor: remove mobile check from user info retrieval in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useEffect } from 'react';
 import ButtonGroup from './components/ButtonGroup';
 import { getUserAccount } from './api';
-import { isMobile } from './utils';
 
 function App() {
   /** 获取用户信息 */
   const getUserInfo = async () => {
     try {
-      if (!isMobile()) return console.log('不是移动端');
       const res = await getUserAccount();
       console.log('res', res);
       if (res.code === 200) {
